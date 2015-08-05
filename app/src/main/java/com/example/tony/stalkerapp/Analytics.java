427fbaaf738e228a49ae093391ce163e4cad17b6
valentinerutto <rutto28@gmail.com>
2015-08-05 21:04:49 +0300

    added analytics() and tracker() to analytics java class

--- app/src/main/java/com/example/tony/stalkerapp/Analytics.java
+++ app/src/main/java/com/example/tony/stalkerapp/Analytics.java
@@ -9,16 +9,25 @@ import com.google.android.gms.analytics.Tracker;
  * Created by valentine on 8/5/15.
  */
 public class Analytics extends Application {
-
     public static GoogleAnalytics analytics;
     public static Tracker tracker;
+
+
+    public static GoogleAnalytics analytics() {
+        return analytics;
+    }
+
+    public static Tracker tracker() {
+        return tracker;
+    }
 
     @Override
     public void onCreate() {
         analytics = GoogleAnalytics.getInstance(this);
         analytics.setLocalDispatchPeriod(1800);
 
-        tracker = analytics.newTracker("UA-65989937-1"); // Replace with actual tracker/property Id
+
+        tracker = analytics.newTracker("UA-6598937-1"); // Replace with actual tracker/property Id
         tracker.enableExceptionReporting(true);
         tracker.enableAdvertisingIdCollection(true);
         tracker.enableAutoActivityTracking(true);
