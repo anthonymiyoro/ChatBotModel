2db77d9f3b0c8d6e5209f933eea1b8368e19042d
valentinerutto <rutto28@gmail.com>
2015-08-05 20:19:46 +0300

    removed tracker initialization from main activity and put it  on analytics java class

--- app/src/main/java/com/example/tony/stalkerapp/Analytics.java
+++ app/src/main/java/com/example/tony/stalkerapp/Analytics.java
@@ -1,7 +1,27 @@
 package com.example.tony.stalkerapp;
 
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Created by valentine on 8/5/15.
  */
-public class Analytics {
+public class Analytics extends Application {
+
+    public static GoogleAnalytics analytics;
+    public static Tracker tracker;
+
+    @Override
+    public void onCreate() {
+        analytics = GoogleAnalytics.getInstance(this);
+        analytics.setLocalDispatchPeriod(1800);
+
+        tracker = analytics.newTracker("UA-65989937-1"); // Replace with actual tracker/property Id
+        tracker.enableExceptionReporting(true);
+        tracker.enableAdvertisingIdCollection(true);
+        tracker.enableAutoActivityTracking(true);
+    }
+
 }
