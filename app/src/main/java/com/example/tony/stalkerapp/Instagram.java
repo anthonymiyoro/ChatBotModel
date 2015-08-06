b73ba20fc87424d60820ef9cfba550dc2ea58b0b
mtnjoroge <mtnjoroge@gmail.com>
2015-08-06 18:26:25 +0300

    added the key and secret

--- app/src/main/java/com/example/tony/stalkerapp/Instagram.java
+++ app/src/main/java/com/example/tony/stalkerapp/Instagram.java
@@ -4,4 +4,8 @@ package com.example.tony.stalkerapp;
  * Created by nganga on 8/6/15.
  */
 public class Instagram {
+
+    public static final String CLIENT_ID = "b91f007786d7405c8f26512b383a0f77";
+    public static final String CLIENT_SECRET = " c85e87eb4d2c484c9ed9c5e5ff5bcf09";
+    public static final String CALLBACK_URL = "http://valentinerutto.github.io/";
 }
