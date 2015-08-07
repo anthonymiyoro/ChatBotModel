cc0a6399564fddf76b05ce3f476afc735e5fe366
valentinerutto <rutto28@gmail.com>
2015-08-07 15:29:33 +0300

    creaed new branch

--- app/src/main/java/com/example/tony/stalkerapp/MainActivity.java
+++ app/src/main/java/com/example/tony/stalkerapp/MainActivity.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "ucwC2VMyvehJQHKUSZtD46uRF";
+    private static final String TWITTER_SECRET = "s2sYX4Q8XuuLVziWP72I5yLrMM0mZZrBSU8fxn9bX2Kjp1Roul";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
 
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
