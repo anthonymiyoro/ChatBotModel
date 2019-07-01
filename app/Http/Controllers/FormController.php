9de69772b4fb254198a2c82303d5497e46ff0651
Anthony Miyoro <amiyoro@gmail.com>
2019-07-01 22:16:29 +0300

    Working POST request

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -23,19 +23,22 @@ class FormController extends BaseController
         print_r($request->input('Twitter_Username'));
         $tname = ($request->input('Twitter_Username'));
 
-        $client = new Client([
-            // You can set any number of default request options.
-            'timeout'  => 200.0,
+           $client = new Client([
+          // You can set any number of default request options.
+          'timeout'  => 2000.0,
+          'auth' => ['admin','Pass@1234']
+      ]);
+
+      $request = $client->post('http://amiyoro2.pythonanywhere.com/analyse_tweet/', [
+            'form_params' => ['twitter_user' => '@migunamiguna'],
+            'twitter_user' => '@migunamiguna'
         ]);
 
-        $res = $client->request('POST', 'http://amiyoro2.pythonanywhere.com/analyse_tweet/', [
-            'form_params' => [
-                'twitter_user' => $tname,
-            ]
-        ]);
-
-        $result= $res->getBody();
-        dd($result);
-
+        echo $request->getStatusCode();
+        // "200"
+        echo '<br/>';
+        echo $request->getHeader('content-type')[0];
+        echo '<br/>';
+        echo $request->getBody();
     }
 }
